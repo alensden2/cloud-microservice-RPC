bca36b0ee7c269219ebe1cf1c1f357c1091f7482
alensden2 <alensanjohn1999@gmail.com>
2023-05-28 16:26:48 -0300

    final code

--- A1/container_P1/middleware.js
+++ A1/container_P1/middleware.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 
 const checkIfFileExists = (req, res, next) => {
-
   const fileName = req.body.file;
-  const filePath = "../files/" + fileName
+  const filePath = "../files/" + fileName;
   if (fileName == null || fileName == "") {
     res.status(400).json({
       file: null,
@@ -19,11 +18,28 @@ const checkIfFileExists = (req, res, next) => {
         });
         return;
       }
-      req.jsonObj = {From: "First"};
-      next()
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+          res.status(500).json({
+            file: fileName,
+            error: "Invalid JSON input.",
+          });
+          return;
+        }
+
+        if (!data.includes(",")) {
+          res.status(200).json({
+            file: fileName,
+            error: "Input file not in CSV format.",
+          });
+          return;
+        }
+
+        req.jsonObj = { From: "First" };
+        next();
+      });
     });
   }
- 
 };
 
 module.exports = {
