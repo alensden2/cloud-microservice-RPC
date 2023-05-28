e9a589e4da46a03c9fbd31ed7a9d3f4bbba93dd0
alensden2 <alensanjohn1999@gmail.com>
2023-05-28 18:42:27 -0300

    fixed bugs

--- A1/container_P1/middleware.js
+++ A1/container_P1/middleware.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const csv = require("csv-parser");
 
 const checkIfFileExists = (req, res, next) => {
   const fileName = req.body.file;
   const filePath = "../files/" + fileName;
-  if (fileName == null || fileName == "") {
+  if (fileName == null || fileName === "") {
     res.status(400).json({
       file: null,
       error: "Invalid JSON input.",
@@ -18,26 +19,43 @@ const checkIfFileExists = (req, res, next) => {
         });
         return;
       }
-      fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-          res.status(500).json({
-            file: fileName,
-            error: "Invalid JSON input.",
-          });
-          return;
-        }
+      let invalidCSV = false;
 
-        if (!data.includes(",")) {
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on("headers", (headers) => {
+          const missingHeaders = headers.filter(
+            (header) => header.trim() === ""
+          );
+          const containsComma = headers.some((header) => header.includes(","));
+          if (missingHeaders.length > 0 || containsComma) {
+            invalidCSV = true;
+          }
+        })
+        .on("data", (row) => {
+          Object.values(row).forEach((value) => {
+            if (value.trim() === "" || value.includes(",")) {
+              invalidCSV = true;
+            }
+          });
+        })
+        .on("error", (error) => {
           res.status(200).json({
             file: fileName,
-            error: "Input file not in CSV format.",
+            error: "Invalid CSV input.",
           });
-          return;
-        }
-
-        req.jsonObj = { From: "First" };
-        next();
-      });
+        })
+        .on("end", () => {
+          if (invalidCSV) {
+            res.status(200).json({
+              file: fileName,
+              error: "Invalid CSV input.",
+            });
+          } else {
+            req.jsonObj = { From: "First" };
+            next();
+          }
+        });
     });
   }
 };
